6f87aca50d9e57aad6eb6e5ef6511fbb4c644420
lramsey <lramsey177@gmail.com>
2014-04-10 15:08:04 -0700

    test 2 rename ticks url to src

--- server/models/klicks.js
+++ server/models/klicks.js
@@ -23,7 +23,7 @@ var klicksSchema = new mongoose.Schema({
     ctrlKey: { type: Boolean, default: false }, // applies only for keypress actions
     metaKey: { type: Boolean, default: false }, // applies only for keypress actions
     shiftKey: { type: Boolean, default: false }, // applies only for keypress actions
-    url: String  // corresponds to the url where the recording happens
+    src: String  // corresponds to the url where the recording happens
   }]
 });
 
