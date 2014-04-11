4c2fca2f44e0d1e81b03334e4e27b982aa083d2e
jyek <justinyek@gmail.com>
2014-04-11 10:02:59 -0700

    Added Made at HR banner, fixed CSS, fixed server routing

--- server-config.js
+++ server-config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var cors = require('cors'); // module for handling Cors headers
+var cors = require('cors');
 var handler = require('./lib/request-handler');
 
 var app = express();
@@ -12,13 +12,10 @@ app.configure(function() {
   app.use(cors());
 });
 
-app.get('/', handler.renderIndex);
-
 app.get('/klicks/:id', handler.getKlick);
 app.get('/klicks', handler.getAllKlicks);
 app.post('/klicks', handler.createKlick);
 
-// app.get(/\/?url=(.+)/, handler.loadKlick);
-// app.get('/*', handler.default);
+app.get('/*', handler.invalidRoute);
 
 module.exports = app;
