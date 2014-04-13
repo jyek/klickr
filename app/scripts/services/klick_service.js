bbf1e8004d14f31bd2bce5a06c6be0969e5218b5
jyek <justinyek@gmail.com>
2014-04-13 15:48:10 -0700

    Minor tweaks

--- app/scripts/services/klick_service.js
+++ app/scripts/services/klick_service.js
@@ -2,9 +2,26 @@
 
 angular.module('klickrApp')
   .service('KlickService', function KlickService($http) {
+
+    /* Get Klicks */
     this.getKlicks = function (){
       return $http.get('/klicks').then(function(klicks){
         return klicks.data;
       });
     };
+
+    /* Create Klick */
+    this.postKlick = function (klick){
+      return $http.post('/klicks', {klick: klick}).then(function(klicks){
+        return klicks.data;
+      });
+    };
+
+    /* Update Klick */
+    this.updateKlick = function (klick){
+      return $http.put('/klicks', {klick: klick}).then(function(klicks){
+        return klicks.data;
+      });
+    };
+
   });
